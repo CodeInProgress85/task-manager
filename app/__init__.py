7da71f2ee03a55650aaf9d6d744c7ae71dcd2d0e
Sushil <sushil.pydev@gmail.com>
2024-12-13 15:12:03 +0545

    pulled repository and modified a few codes.

--- app/__init__.py
+++ app/__init__.py
@@ -1,4 +1,4 @@
-from flask import Flask, redirect, flash, url_for, request
+from flask import Flask, redirect, url_for, request
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_login import LoginManager
